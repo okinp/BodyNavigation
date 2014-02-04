9e9ce35a961a6b50ed64a181e7af5627824f69a2
Nikolas Psaroudakis <psaroudakis@gmail.com>
2014-02-04 21:54:20 +0200

    Initial commit

---
+++ src/org/oem/bodynavigation/MainActivity.java
@@ -0,0 +1,63 @@
+package org.oem.bodynavigation;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.*;
+
+public class MainActivity extends FragmentActivity {
+	 
+    // Google Map
+    private GoogleMap googleMap;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+            	googleMap.setMyLocationEnabled(true);
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bodynavigation_menu, menu);
+        return true;
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+ 
+}
---
+++ src/org/oem/bodynavigation/SearchScreen.java
@@ -0,0 +1,14 @@
+package org.oem.bodynavigation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SearchScreen extends Activity {
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+	}
+	
+};
